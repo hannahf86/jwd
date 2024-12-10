c5e15a06de4930fb910a201dd3b0e4f8f400b30a
hannahf86 <hannahfeehan.design@gmail.com>
2024-12-10 17:01:36 +0000

    responsive design complete

--- src/Pages/Reviews.jsx
+++ src/Pages/Reviews.jsx
@@ -25,7 +25,7 @@ const Reviews = () => {
         {/* COL 02 */}
         <div className="col-start-1 md:col-start-2 md:row-start-1 lg:pt-32">
           {/* REVIEW 01*/}
-          <div className="bg-white rounded-lg p-12 mx-36 mb-12">
+          <div className="bg-white rounded-lg p-4 md:p-12 lg:mx-36 mb-12">
             <div className="flex justify-center my-6">
               <FaQuoteLeft size={20} className="mr-2 text-accent" />
               <h2 className="text-lg text-center font-bold bg-accent text-white p-2">
@@ -33,13 +33,13 @@ const Reviews = () => {
               </h2>
               <FaQuoteRight size={20} className="ml-2  text-accent" />
             </div>
-            <p className="text-green font-bold text-md text-center my-8 ">
+            <p className="text-green font-bold text-md text-center mt-8 ">
               &quot;Amazing person to work with. Very friendly and thorough. I
               would definitely recommend 😊&quot;
             </p>
-            <div className="m-auto flex items-center justify-between">
+            <div className="m-auto flex flex-col items-center justify-between mt-4">
               <div className="flex items-center">
-                <div className="flex text-yellow-500 ml-4">
+                <div className="flex text-yellow-500 lg:ml-4 my-2">
                   <TiStarFullOutline size={30} />
                   <TiStarFullOutline size={30} />
                   <TiStarFullOutline size={30} />
@@ -47,35 +47,36 @@ const Reviews = () => {
                   <TiStarFullOutline size={30} />
                 </div>
               </div>
-              <p className="text-black">Matthew, Oct 2024</p>
+              <p className="text-black/50 italic">Matthew, Oct 2024</p>
             </div>
           </div>
-        </div>
 
-        {/* REVIEW 02*/}
-        <div className="bg-white rounded-lg p-12 mx-36">
-          <div className="flex justify-center my-6">
-            <FaQuoteLeft size={20} className="mr-2 text-accent" />
-            <h2 className="text-lg text-center font-bold bg-accent text-white p-2">
-              well happy
-            </h2>
-            <FaQuoteRight size={20} className="ml-2  text-accent" />
-          </div>
-          <p className="text-green font-bold text-md text-center my-8 ">
-            &quot;Thankbyou for all the work you have put in well happy with my
-            website&quot;
-          </p>
-          <div className="m-auto flex items-center justify-between">
-            <div className="flex items-center">
-              <div className="flex text-yellow-500 ml-4">
-                <TiStarFullOutline size={30} />
-                <TiStarFullOutline size={30} />
-                <TiStarFullOutline size={30} />
-                <TiStarFullOutline size={30} />
-                <TiStarFullOutline size={30} />
-              </div>
+          {/* REVIEW 02*/}
+          <div className="bg-white rounded-lg p-4 md:p-12 lg:mx-36 mb-12">
+            <div className="flex justify-center my-6">
+              <FaQuoteLeft size={20} className="mr-2 text-accent" />
+              <h2 className="text-lg text-center font-bold bg-accent text-white p-2">
+                ...the whole process really easy...
+              </h2>
+              <FaQuoteRight size={20} className="ml-2  text-accent" />
             </div>
-            <p className="text-black">Dougie, Oct 2024</p>
+            <p className="text-green font-bold text-md text-center mt-8 ">
+              &quot;Thanks for help with my new website from start to finish
+              Hannah was really helpful and made the whole process really easy …
+              Would highly recommend&quot;
+            </p>
+            <div className="m-auto flex flex-col items-center justify-between mt-4">
+              <div className="flex items-center">
+                <div className="flex text-yellow-500 lg:ml-4 my-2">
+                  <TiStarFullOutline size={30} />
+                  <TiStarFullOutline size={30} />
+                  <TiStarFullOutline size={30} />
+                  <TiStarFullOutline size={30} />
+                  <TiStarFullOutline size={30} />
+                </div>
+              </div>
+              <p className="text-black/50 italic">Mike, Oct 2024</p>
+            </div>
           </div>
         </div>
       </div>
